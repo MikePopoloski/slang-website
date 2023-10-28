0478a3f2965efec84d84fe85442eb5788a6bc47f
MikePopoloski <mike@popoloski.com>
2023-10-28 10:42:51 -0400

    Doc deploy script: remove target exe before copying

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 tar -xvf upload/slang.tar.gz
 rm -rf /var/www/sv-lang.com/html
 cp -r build/docs/html/ /var/www/sv-lang.com/html/
+rm -f /home/ubuntu/slang-website/slang
 cp build/bin/slang /home/ubuntu/slang-website/
 rm -rf upload
 rm -rf build
